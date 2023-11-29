0bfc15876c294d4b452e2bc7220276e5b14c1b46
KARTHICK877 <kmass8754@gmail.com>
2023-11-29 12:02:09 +0530

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { addUser } from "@services/APIservices.js";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@services": path.resolve(__dirname, "src/Services"),
+      '@services': path.resolve(__dirname, 'src/Services'),
     },
   },
-})
+});
